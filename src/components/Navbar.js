b8f02a5ce4f56e72df527cc7778616588cb52cf7
Rashidaakter1 <rashidaakterchadni@gmail.com>
2022-07-02 05:05:16 +0600

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,47 +3,54 @@ import { Link } from "react-router-dom";
 import { FaAlignJustify } from "react-icons/fa";
 
 const Navbar = () => {
-    let [open, setOpen] = useState(false);
+   
     return (
 
 
-        <div className='shadow-md w-full fixed top-0 left-0'>
-
-            <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-                <div>
-                    <Link to='/home'> <img className='w-10' src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1428212405/u438ahwi43tw6l5oe3bl.png" alt="" /></Link>
+        <div class="navbar bg-slate-200">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a className='text-gray-900 hover:text-gray-500' as={Link} href="/toDoList">To-do list</a></li>
+                        
+                        <li><a className='text-gray-900 hover:text-gray-500' as={Link} href="/completedTask">Completed task</a></li>
+                        <li><a className='text-gray-900 hover:text-gray-500' as={Link} href="/calender">Calender</a></li>
+                    </ul>
                 </div>
-
-                <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-                    <div className={open ? 'close':'menu'}> <FaAlignJustify ></FaAlignJustify></div>
-                </div>
-                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
-                    <li className='md:ml-8 sm:m md:my-0 my-7'><a className='text-gray-900 hover:text-gray-500' as={Link} href="/toDoList">To-do list</a></li>
-                    <li className='md:ml-8  md:my-0 my-7'><a className='text-gray-900 hover:text-gray-500' as={Link} href="/completedTask">Completed task</a></li>
-                    <li className='md:ml-8  md:my-0 my-7'><a className='text-gray-900 hover:text-gray-500' as={Link} href="/calender">Calender</a></li>
-
-                </ul>
-
+                <Link to='/home' className='font-[poppins] pl-3 '> <img className='w-10' src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1428212405/u438ahwi43tw6l5oe3bl.png" alt="" /></Link>
             </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                <li><a className='text-gray-900 hover:text-gray-500' as={Link} href="/toDoList">To-do list</a></li>
+                        
+                        <li><a className='text-gray-900 hover:text-gray-500' as={Link} href="/completedTask">Completed task</a></li>
+                        <li><a className='text-gray-900 hover:text-gray-500' as={Link} href="/calender">Calender</a></li>
+                </ul>
+            </div>
+            
 
-
-
-
+            
             {/* <div className='md:flex items-center justify-between  md:mx-6 text-xl py-5 '>
                 <div className=''>
-                    <Link to='/home' className='font-[poppins] pl-3 '> <img className='w-10' src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1428212405/u438ahwi43tw6l5oe3bl.png" alt="" /></Link>
+                    
                 </div>
                 <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
                     <div className=''> <FaAlignJustify ></FaAlignJustify></div>
                 </div>
                 <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
                     <li className='md:ml-8 sm:m md:my-0 my-7'><a className='text-gray-900 hover:text-gray-500' as={Link} href="/toDoList">To-do list</a></li>
-                    <li className='md:ml-8  md:my-0 my-7'><a className='text-gray-900 hover:text-gray-500' as={Link} href="/completedTask">Completed task</a></li>
-                    <li className='md:ml-8  md:my-0 my-7'><a className='text-gray-900 hover:text-gray-500' as={Link} href="/calender">Calender</a></li>
+                    <li className='md:ml-8  md:my-0 my-7'></li>
+                    <li className='md:ml-8  md:my-0 my-7'></li>
 
                 </ul>
             </div> */}
+        
         </div>
+
+
     );
 };
 
